06dae73a090b3aa35d906cc6d87a11384be7a5e2
yanina.perez <yanina.perez@mediamonks.com>
2022-09-06 13:35:32 -0300

    Add style to login page

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -10,6 +10,15 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const switchers = document.querySelectorAll('.switcher');
+
+    switchers.forEach(item => {
+      item.addEventListener('click', function () {
+        switchers.forEach(item2 => item2.parentElement?.classList.remove('is-active'))
+        item.parentElement?.classList.add('is-active')
+      })
+    })
+
   }
 
 }
