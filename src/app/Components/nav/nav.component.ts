7006368b563f9cc8aae88a6167e385b9dfadc326
yanina.perez <yanina.perez@mediamonks.com>
2022-09-08 11:21:57 -0300

    Set menu for mobile

--- src/app/Components/nav/nav.component.ts
+++ src/app/Components/nav/nav.component.ts
@@ -12,4 +12,25 @@ export class NavComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openMenu() {
+    let x = Array.from(document.getElementsByClassName('nav-item') as HTMLCollectionOf<HTMLElement>)
+
+    x.forEach( link => {
+       if (link && link.style.display === "block") {
+        link.style.display = "none";
+      } else {
+        link!.style.display = "block";
+      } 
+    } )
+
+
+  }
+
+  closeMenu(){
+    if(window.matchMedia("(max-width: 768px)").matches){
+      this.openMenu();
+    }
+
+  }
+
 }
